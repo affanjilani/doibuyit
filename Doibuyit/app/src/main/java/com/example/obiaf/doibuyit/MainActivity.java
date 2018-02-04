2ec9e63c41fc5d9acaf7e5ee88a7aac2552e1cb9
affanjilani <obiaffan@gmail.com>
2018-02-03 21:26:57 -0500

    Overview layout almost done

--- Doibuyit/app/src/main/java/com/example/obiaf/doibuyit/MainActivity.java
+++ Doibuyit/app/src/main/java/com/example/obiaf/doibuyit/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.obiaf.doibuyit;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.akexorcist.roundcornerprogressbar.RoundCornerProgressBar;
+import com.mikhaellopez.circularprogressbar.CircularProgressBar;
 
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    Context context = this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RoundCornerProgressBar progress1 = (RoundCornerProgressBar) findViewById(R.id.weeklyProgress);
+        progress1.setProgressColor(Color.parseColor("#FFFFFF"));
+        progress1.setProgressBackgroundColor(ContextCompat.getColor(this.context, R.color.moneyGreen));
+        progress1.setMax(250);
+        progress1.setProgress(150);
+
+        CircularProgressBar circularProgressBar = (CircularProgressBar)findViewById(R.id.monthlyProgress);
+        int animationDuration = 2500; // 2500ms = 2,5s
+        circularProgressBar.setProgressWithAnimation(65, animationDuration); // Default duration = 1500ms
+
     }
+
 }
